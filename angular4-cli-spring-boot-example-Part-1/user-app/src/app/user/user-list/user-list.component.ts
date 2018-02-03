66cecbac56f9e3a08bbfe48947536aba658eb4d3
pradippa <pradippa@LIN51004853.corp.capgemini.com>
2018-02-03 15:30:25 +0530

    First Angular app

---
+++ angular4-cli-spring-boot-example-Part-1/user-app/src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: any;
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/users').subscribe(data => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+
+
+
+}
