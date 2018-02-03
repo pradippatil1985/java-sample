70fdfd9db7baed0ce6788dd49b6e3268da1f3319
pradippa <pradippa@LIN51004853.corp.capgemini.com>
2018-02-03 16:33:24 +0530

    commit

---
+++ mapreduce/src/main/java/com/pradip/spring/data/apachehadoop/Main.java
@@ -0,0 +1,13 @@
+package com.pradip.spring.data.apachehadoop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Patil Pradip
+ */
+public class Main {
+	public static void main(String[] arguments) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+}
